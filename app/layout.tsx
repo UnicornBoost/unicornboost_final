be1181191fe044bb7c66f84d20c6c499d42db1b6
UnicornBoost <148924741+UnicornBoost@users.noreply.github.com>
2023-11-17 17:56:22 +0000

    Update layout.tsx
    I have made some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
+import Script from 'next/script'
+const GTM_ID = 'GTM-MTP3GXJ6';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +20,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+    </Script>
+      
       <body className={inter.className}>
         <Navbar />
         {children}
         <Footer />
+         <noscript
+        dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+        }}
+    />
       </body>
     </html>
   );
